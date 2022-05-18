086f1017643331931ccfa98b39b9f3f3249ee0ae
Hossam <Matrixhoss2@gmail.com>
2022-05-18 19:53:57 +0200

    add search

--- src/components/AppointmentList.tsx
+++ src/components/AppointmentList.tsx
@@ -5,12 +5,14 @@ import {
   CardContent,
   CardHeader,
   List,
+  TextField,
   Typography,
 } from '@material-ui/core';
 import { formatDateRange } from 'utils/date';
 import { practitionersSelectors } from 'store/practitioners';
 import { patientsSelectors } from 'store/patients';
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import { useState } from 'react';
 
 const AppointmentList = ({
   practitioners,
@@ -19,6 +21,9 @@ const AppointmentList = ({
   handleDeleteAppointment,
   handleEditAppointment,
 }) => {
+  //state
+  const [search, setSearch] = useState('');
+
   // utils functions
   const getAppointmentPractitioner = (id) =>
     practitionersSelectors.selectById(practitioners, id);
@@ -26,58 +31,84 @@ const AppointmentList = ({
     patientsSelectors.selectById(patients, id);
   const getTimeSlotDatacy = (id: string) => `timeslot-${id}`;
 
+  const searchArray = (array, search) => {
+    if (!search) return array;
+
+    return array.filter((a) => {
+      const patient = getAppointmentPatient(a.patientId);
+      const practitioner = getAppointmentPractitioner(a.practitionerId);
+
+      return (
+        patient.firstName.toLowerCase().includes(search.toLowerCase()) ||
+        patient.lastName.toLowerCase().includes(search.toLowerCase()) ||
+        practitioner.firstName.toLowerCase().includes(search.toLowerCase()) ||
+        practitioner.lastName.toLowerCase().includes(search.toLowerCase())
+      );
+    });
+  };
+
   return (
     appointments && (
-      <List className="appointments" datacy="timeslot-list">
-        {appointments.map((item) => {
-          const practitioner = getAppointmentPractitioner(item.practitionerId);
-          const patient = getAppointmentPatient(item.patientId);
-          return (
-            <Card
-              key={item.id}
-              datacy={getTimeSlotDatacy(item.id)}
-              className="appointment__item btn"
-            >
-              <CardHeader
-                avatar={<CalendarTodayIcon />}
-                title={
-                  <Typography datacy={`${getTimeSlotDatacy(item.id)}-range`}>
-                    {formatDateRange({
-                      from: new Date(item.startDate),
-                      to: new Date(item.endDate),
-                    })}
+      <>
+        <TextField
+          value={search}
+          placeholder="Search by name"
+          onChange={(e) => setSearch(e.target.value)}
+          variant="outlined"
+        />
+        <List className="appointments" datacy="timeslot-list">
+          {searchArray(appointments, search).map((item) => {
+            const practitioner = getAppointmentPractitioner(
+              item.practitionerId,
+            );
+            const patient = getAppointmentPatient(item.patientId);
+            return (
+              <Card
+                key={item.id}
+                datacy={getTimeSlotDatacy(item.id)}
+                className="appointment__item btn"
+              >
+                <CardHeader
+                  avatar={<CalendarTodayIcon />}
+                  title={
+                    <Typography datacy={`${getTimeSlotDatacy(item.id)}-range`}>
+                      {formatDateRange({
+                        from: new Date(item.startDate),
+                        to: new Date(item.endDate),
+                      })}
+                    </Typography>
+                  }
+                />
+                <CardContent>
+                  <Typography>
+                    Practitioner :
+                    {`${practitioner?.firstName} ${practitioner?.lastName}`}
                   </Typography>
-                }
-              />
-              <CardContent>
-                <Typography>
-                  Practitioner :
-                  {`${practitioner?.firstName} ${practitioner?.lastName}`}
-                </Typography>
-                <Typography>
-                  Patient : {`${patient?.firstName} ${patient?.lastName}`}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  variant="outlined"
-                  onClick={() => handleEditAppointment(item)}
-                >
-                  Edit
-                </Button>
-                <Button
-                  style={{ backgroundColor: '#f44949', color: '#fff' }}
-                  onClick={() => {
-                    handleDeleteAppointment(item.id);
-                  }}
-                >
-                  Delete
-                </Button>
-              </CardActions>
-            </Card>
-          );
-        })}
-      </List>
+                  <Typography>
+                    Patient : {`${patient?.firstName} ${patient?.lastName}`}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    variant="outlined"
+                    onClick={() => handleEditAppointment(item)}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    style={{ backgroundColor: '#f44949', color: '#fff' }}
+                    onClick={() => {
+                      handleDeleteAppointment(item.id);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </CardActions>
+              </Card>
+            );
+          })}
+        </List>
+      </>
     )
   );
 };
